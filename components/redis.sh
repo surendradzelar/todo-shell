189b60440247a76683f80cd94577c6611ca9379c
zssurendradzelar <surendradudi432@gmail.com>
2021-07-22 13:56:46 +0530

    first

--- components/redis.sh
+++ components/redis.sh
@@ -6,11 +6,10 @@ OS_PREREQ
 Head "Install Redis"
 apt install redis-server -y &>>$LOG
 Stat $?
-
 Head "Update Redis Listen Address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
 Stat $?
 
 Head "Start Redis Service"
-systemctl restart redis && systemctl status redis &>>$LOG
+systemctl restart redis
 Stat $?
