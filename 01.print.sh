aab29ba29dd80223f0d29c4047d8376d7ca58b87
zssurendradzelar <surendradudi432@gmail.com>
2021-07-29 10:53:39 +0530

    first

---
+++ 01.print.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Hello World"
+
+echo hai
+echo surendra
+
+echo hai\n surendra
+
+echo -e "\e[34m hai hello]"
+echo "hai hello"
+
+echo -e "\e[34m hai hello\e[0m"
+echo "hai hello"
