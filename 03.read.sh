320cc9e97b3acc96bcbcddc321c910f359dea4c0
zssurendradzelar <surendradudi432@gmail.com>
2021-07-29 12:19:25 +0530

    first

--- 03.read.sh
+++ 03.read.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -p "enter name: ", name
-read -p "enter age: ", age 
+read -p "enter name: " name
+read -p "enter age: " age 
 
-echo -e  "your name="\e[32m$name\e[0m", your age="\e[34m$age\e[0m"
+echo -e  "your name="\e[32m$name\e[0m", your age="\e[34m$age\e[0m" "
