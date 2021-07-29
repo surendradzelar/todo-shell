89b8efda08ebe312c5b17a3cad90229d2bb180a4
zssurendradzelar <surendradudi432@gmail.com>
2021-07-29 14:55:26 +0530

    first

--- 06.function.sh
+++ 06.function.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 function surendra(){
+    echo a - $a
+    b=20
     echo hello from surendra
 }
 
 surendra2(){
     echo hello from surendra2
 }
+
+
+surendra
+surendra2
+
+a=10
+echo b -$b
